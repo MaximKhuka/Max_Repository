275561cdc8171855380ca0bd3a84834a5bae9b4b
Maksym Khuka <maksym.khuka@matrixcare.com>
2024-04-08 13:04:46 +0200

    homework_6

---
+++ HOMEWORK6/homework_6.js
@@ -0,0 +1,109 @@
+// *** task1 ***
+
+let test1 = document.getElementById("test").innerHTML = "Last";
+console.log(test1);
+
+let test2 = document.querySelector("div");
+let test3 = test2.innerHTML = "Last";
+console.log(test2);
+
+
+
+
+
+// *** task2 ***
+
+// let pic = document.getElementsByClassName("image").src = "cat.jpg";
+// alert(pic);
+
+
+
+
+
+// *** task3 ***
+
+// var paragraphs = document.querySelectorAll('#text p');
+// paragraphs.forEach(function(paragraph, index) {
+//     console.log("Selector text " + index + ": " + paragraph.textContent);
+// });
+
+
+
+
+
+// *** task4 ***
+
+// let list1 = document.getElementById('list');
+
+// alert((list1.children[0].innerHTML));
+// alert((list1.children[4].innerHTML));
+// alert((list1.children[1].innerHTML));
+// alert((list1.children[3].innerHTML));
+// alert((list1.children[2].innerHTML));
+
+// alert(list.firstElementChild.innerHTML);
+// alert(list.lastElementChild.innerHTML);
+// alert(list.firstElementChild.nextElementSibling.innerHTML);
+// alert(list.lastElementChild.previousElementSibling.innerHTML);
+// alert(list.lastElementChild.previousElementSibling.previousElementSibling.innerHTML);
+
+
+
+
+
+// *** task5 ***
+
+// let header1 = document.querySelector('h1').style.backgroundColor = "lightGreen";
+
+// let paragraph1 = document.getElementById('myDiv');
+// let pp2 = paragraph1.children[0].style.fontSize = "20px";
+// let pp1 = paragraph1.children[0].style.fontWeight = "bold";
+// let pp3 = paragraph1.children[1].style.fontSize = "20px";
+// let pp4 = paragraph1.children[1].style.color = "red";
+// let pp5 = paragraph1.children[2].style.fontSize = "20px";
+// let pp6 = paragraph1.children[2].style.textDecoration = "underline";
+// let pp7 = paragraph1.children[3].style.fontSize = "20px";
+// let pp8 = paragraph1.children[3].style.fontStyle = "italic";
+
+// let lis = document.getElementById('myList');
+// lis.innerHTML = "Makemehorizontal!";
+
+// let spa = document.querySelector('span').style.color = "transparent";
+
+
+
+
+
+// *** task6 ***
+
+// let info1 = prompt("Please, enter your name");
+// let info2 = prompt("Please, enter your surname");
+
+// let f1Val = document.getElementById('input1').value;
+// input1.value = info1;
+
+// let f2Val = document.getElementById('input2').value;
+// input2.value = info2;
+
+// info1 = document.getElementById('input1').value;
+// input2.value = info1;
+// input1.value = info2;
+
+
+
+
+
+// *** task7 ***
+
+// var body = document.createElement('body');
+// var main = document.createElement('main');
+// var div = document.createElement('div');
+// var paragraph = document.createElement('p');
+
+// main.className = 'mainClass check item';
+// div.id = 'myDiv';
+// paragraph.textContent = 'First paragraph';
+// div.appendChild(paragraph);
+// main.appendChild(div);
+// body.appendChild(main);
+// document.documentElement.appendChild(body);
